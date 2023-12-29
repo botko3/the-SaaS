fb9ce42bf95e88268ba2efdca0160c1dc7030ffb
botko3 <112144946+botko3@users.noreply.github.com>
2023-12-30 00:03:36 +0800

    set up database using prisma and mysql

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,15 +1,33 @@
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/types/server';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
  
 export const appRouter =  router ({
   authCallback : publicProcedure.query(async ()=>{
     const { getUser } = getKindeServerSession()
     const user = await getUser()
 
+
+    if(!user ||!user?.id ||!user.email) throw new TRPCError({code:'UNAUTHORIZED'})
+
     //check if the user is in database
 
-    if(!user?.id ||!user.email) throw new TRPCError({code:'UNAUTHORIZED'})
+
+    const dbUser = await db.user.findFirst({
+      where:{
+        id: user.id
+      }
+    })
+
+    if(!dbUser){
+      await db.user.create({
+        data:{
+          id: user.id,
+          email: user.email
+        }
+      })
+    }
 
     return {success:true}
   })
